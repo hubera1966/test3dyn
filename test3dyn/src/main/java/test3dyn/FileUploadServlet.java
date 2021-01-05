3d3f8081c45a3e3c8e6f732e7d71c84af831d71b
Albert Huber <albert.huber@printus.de>
2021-01-05 10:46:01 +0100

    file upload

---
+++ test3dyn/src/main/java/test3dyn/FileUploadServlet.java
@@ -0,0 +1,35 @@
+package test3dyn;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+/* The Java file upload Servlet example */
+
+@WebServlet(name = "FileUploadServlet", urlPatterns = { "/fileuploadservlet" })
+@MultipartConfig(
+  fileSizeThreshold = 1024 * 1024 * 10, // 10 MB
+  maxFileSize = 1024 * 1024 * 20,      // 20 MB
+  maxRequestSize = 1024 * 1024 * 100   // 100 MB
+)
+public class FileUploadServlet extends HttpServlet {
+
+  private static final long serialVersionUID = 1L;
+
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    /* Receive file uploaded to the Servlet from the HTML5 form */
+    Part filePart = request.getPart("file");
+    String fileName = filePart.getSubmittedFileName();
+    for (Part part : request.getParts()) {
+      part.write("/usr/share/nginx/html/" + fileName);
+    }
+    response.getWriter().print("The file uploaded sucessfully.");
+  }
+
+}
+
+
+
+
+
